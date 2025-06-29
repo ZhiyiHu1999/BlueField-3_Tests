9550d329d6786a54377330f8d77950e38b391285
Zhiyi Hu <zhiyihu@ZhiyideMacBook-Air.local>
2025-06-29 14:41:57 +0800

    draft of sample

---
+++ doca_test/samples/doca_common_test/graph_encryption_rdma_send/graph.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include <doca_mmap.h>
+#include <doca_buf.h>
+#include <doca_buf_inventory.h>
+#include <doca_ctx.h>
+#include <doca_pe.h>
+#include <doca_dma.h>
+#include <doca_graph.h>
+#include <doca_types.h>
+#include <doca_log.h>
+
+#include <samples/common.h>
